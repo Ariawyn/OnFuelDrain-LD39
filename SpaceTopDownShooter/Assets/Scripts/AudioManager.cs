51ce7d038bf07aa626410881429c0c2c4d971062
Ariawyn <ariagudlaug@gmail.com>
2017-07-29 20:27:07 +0100

    Basic audio management system is done

---
+++ SpaceTopDownShooter/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour {
+
+	// Sounds array, this should be set up through the inspector, useful for looping through audio instances
+	public Sound[] sounds;
+
+	// Dictionary of sounds to their name, useful for playing without having to find the audio track
+	// This is initialized in a function, not through inspector
+	Dictionary<string, Sound> library;
+
+	// Volume and pitch values
+	private float musicVolume = 1f;
+	private float effectsVolume = 1f;
+	private float defaultPitch = 1f;
+
+	// INIT manager
+	void Awake() {
+		this.initLibrary();
+		this.loadSavedSoundSettings();
+	}
+
+	void Start() {
+		this.Play("Theme");
+	}
+
+	public void Play(string name) {
+		if(this.library.ContainsKey(name)) {
+			Sound s = this.library[name];
+			if(s.isSFX) {
+				// Randomize the effects volume and pitch by their random values
+				s.source.volume = this.effectsVolume * (1 + Random.Range(-s.randomVolumeValue / 2f, s.randomVolumeValue / 2f));
+				s.source.pitch = this.defaultPitch * (1 + Random.Range(-s.randomPitchValue / 2f, s.randomPitchValue / 2f));
+				
+				// Play the effect
+				s.source.Play();
+				return;
+			} else {
+				// Play the song
+				s.source.Play();
+			}
+			return;
+		}
+
+		// If we get to this point, then we have no sound file for the given name
+		Debug.Log("No sound file found for name: " + name);
+	}
+
+	public void setMusicVolume(float volume) {
+		// Set new music volume value
+		this.musicVolume = volume;
+
+		// Change all music sound instances to new volume
+		this.updateVolume();
+	}
+
+	public void setEffectsVolume(float volume) {
+		// Set new music volume value
+		this.effectsVolume = volume;
+
+		// Change all effects sound instances to new volume
+		this.updateVolume();
+	}
+
+	// Loops through and resets the sound sources to the correct current volume
+	private void updateVolume() {
+		foreach(Sound s in sounds) {
+			if(s.isSFX) {
+				s.source.volume = this.effectsVolume;
+			} else {
+				s.source.volume = this.musicVolume;
+			}
+		}
+	}
+
+	// Gets settings related to audio, if none saved, then everything is still set to default
+	private void loadSavedSoundSettings() {
+		// TODO: Read sound settings from player prefs overriding default values for music and effects volume
+		return;
+	}
+
+	// Saves sound settings to player prefs
+	private void saveSoundSettings() {
+		// TODO: Store / save sound settings into player prefs
+		return;
+	}
+
+	// Initializes sound dictionary "library" using the sounds array set through inspector
+	private void initLibrary() {
+		// Instantiate a new dictionary
+		this.library = new Dictionary<string, Sound>();
+
+		// Add all sound instances into dictionary
+		foreach(Sound s in sounds) {
+			// Add sound to library
+			this.library[s.name] = s;
+
+			// We also want to create all the audio sources here
+			s.source = this.gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+			s.source.loop = s.isLooping;
+		}
+	}
+}
---
+++ SpaceTopDownShooter/Assets/Scripts/Sound.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+// public sound class for handling BGM and SFX
+[System.Serializable]
+public class Sound {
+	
+	// Basic constructor
+	public Sound(string name, float volume, float pitch, AudioClip clip) {
+		this.name = name;
+		this.volume = volume;
+		this.pitch = pitch;
+		this.clip = clip;
+	}
+
+	// Name of track
+	public string name;
+
+	// Boolean for seeing if we want the track to loop
+	public bool isLooping;
+
+	// Boolean for seeing if this sound instance is a sound effect
+	public bool isSFX;
+
+	// Random values for making effects not sound repetitive, only used if isSFX is true
+	[Range(0f, 0.5f)]
+	public float randomVolumeValue = 0.1f;
+	[Range(0f, 0.5f)]
+	public float randomPitchValue = 0.1f;
+
+	// Audio Clip instance for the audio track
+	public AudioClip clip;
+
+	// Audio source, where the audio plays from
+	[HideInInspector]
+	public AudioSource source;
+
+	// Volume as a range between 0 - 1
+	[Range(0f, 1f)]
+	public float volume;
+
+	// Pitch as a range between 0.1 - 3, 1 is normal pitch
+	[Range(0.1f, 3f)]	
+	public float pitch;
+}
