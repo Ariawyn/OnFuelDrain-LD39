5f0cbc320e8c01d3ef1652571a41771d6b01e55e
reizoukin <reizoukin@users.noreply.github.com>
2017-07-31 23:17:58 +0100

    health pickups now have 30 second timers

--- SpaceTopDownShooter/Assets/Scripts/Pickups/HealthPickup.cs
+++ SpaceTopDownShooter/Assets/Scripts/Pickups/HealthPickup.cs
@@ -6,6 +6,22 @@ public class HealthPickup : MonoBehaviour {
 
 	public float healthValue = 20;
 
+	public float timerDefault = 30;
+
+	float timer = 0;
+
+	void OnEnable() {
+		timer = timerDefault;
+	}
+
+	void Update() {
+		timer -= Time.deltaTime;
+
+		if (timer <= 0)
+			SimplePool.Despawn (this.gameObject);
+	}
+
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			other.gameObject.GetComponent<Player>().SendMessage("UpdateHealth",healthValue);
